48c729c0326aa10774b4789a856e68228366f112
UnknownMonk <smodcartoon@gmail.com>
2019-07-12 11:55:35 -0400

    ex1 done and passing

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -23,7 +23,8 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+    while valid_proof(last_proof, proof):
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -37,8 +38,11 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...999123456, new hash 123456888...
     """
 
-    # TODO: Your code here!
-    pass
+    last_hash = str(last_hash)
+    guess_first_proof = str(proof).encode()
+
+    guess_proof = hashlib.sha256(guess_first_proof).hexdigest()
+    return last_hash[-6:] == guess_proof[:6]
 
 
 if __name__ == '__main__':
--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,13 +7,34 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    if length <= 1:
+        return None
 
-    """
-    YOUR CODE HERE
-    """
+    ht = HashTable(length)
 
-    return None
+    for i in range(length):
+        # check if a key (element from weights arr) is already in hash table
+        key_exist = hash_table_retrieve(ht, weights[i])
+        if (key_exist == 0 or key_exist) and (limit - weights[i] == weights[i]):
+            # 2 same elements, each equal to limit/2 => element + element = limit
+            return (i, key_exist)
+        hash_table_insert(ht, weights[i], i)
+
+    # placeholder for keys that sum to limit
+    keys = []
+
+    # search for key, which adds to limit with another key => limit-key = another key
+    for i in range(length):
+        key_value = hash_table_retrieve(ht, limit-weights[i])
+        if key_value:
+            keys.append(weights[i])
+            keys.append(limit-weights[i])
+            break
+
+    indexes = [weights.index(keys[0]), weights.index(keys[1])]
+    indexes.sort(reverse=True)
+
+    return (indexes[0], indexes[1])
 
 
 def print_answer(answer):
